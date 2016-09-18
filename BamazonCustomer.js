00848b30b0fbcc4aa340836cf8b8563759a89a4d
Kai <huikaizhong@yahoo.com>
2016-09-18 00:21:19 -0400

    add on prompt

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,4 +1,6 @@
 var mysql = require('mysql');
+var inquirer = require('inquirer');
+
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -12,13 +14,37 @@ connection.connect(function(err) {
     console.log("connected as id " + connection.threadId);
 });
 
-connection.query("SELECT * FROM bamazon.products",function(err,rows){
+connection.query("SELECT ItemID, ProductName, Price FROM bamazon.products",function(err,rows){
 	if (err){
 		throw err;
 
 	}
 	console.log(rows);
+    id();
 })
 	connection.end();
+
+var id = function() {
+    inquirer.prompt({
+        name: "ID",
+        type: "input",
+        message: "What do you want to buy",
+        default: "Enter produt ID"
+    }).then(function(answer) {
+        quantity();
+    })
+};
+
+var quantity = function() {
+    inquirer.prompt({
+        name: "quantity",
+        type: "input",
+        message: "How many do you want to buy",
+        default: "Enter quantity #"
+    }).then(function(answer) {
+        
+    })
+};
+
 
 
